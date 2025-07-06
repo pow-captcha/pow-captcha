a744d97bb9600b6c7dc937c28eab8ceeef56a201
Thomas Heck <t@b128.net>
2025-07-06 18:41:41 +0200

    test(server): add server tests

---
+++ pkgs/server/src/lib.spec.ts
@@ -0,0 +1,31 @@
+import { describe, it, expect } from "@jest/globals";
+import { wire } from "@pow-captcha/shared";
+import * as server from "./lib";
+
+const SECRET = "e2c0b4ab-a215-4b36-bba8-19fae1601045";
+
+describe("server", () => {
+	it("createChallenges ok", async () => {
+		const challengesSigned = await server.createChallenges({}, SECRET);
+
+		await wire.verifyAndDeserializeData(
+			challengesSigned,
+			wire.challengeSchema,
+			SECRET,
+		);
+	});
+
+	it("createChallenges wrong secret", async () => {
+		const challengesSigned = await server.createChallenges({}, SECRET);
+
+		await expect(
+			(async () => {
+				await wire.verifyAndDeserializeData(
+					challengesSigned,
+					wire.challengeSchema,
+					"wrong-secret",
+				);
+			})(),
+		).rejects.toThrow("Signed data verification failed, hash mismatch");
+	});
+});
